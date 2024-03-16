3fe2a07e39508357abd99194f96b75afb7a73f74
funiris <162334368+funiris@users.noreply.github.com>
2024-03-16 18:28:38 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,21 +3,29 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName('John');
+    const result = myClass.addStudent(student);
+    assert.strictEqual(result, 0, "Student should be added successfully");
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName('Jane');
+    myClass.addStudent(student);
+    const retrievedStudent = myClass.getStudentById(0);
+    assert.strictEqual(retrievedStudent.getName(), 'Jane', "Retrieved student name should be 'Jane'");
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName('Doe');
+    assert.strictEqual(student.getName(), 'Doe', "Student name should be 'Doe'");
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    assert.strictEqual(student.getName(), '', "Student name should be an empty string initially");
 });
