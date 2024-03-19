320e2e9bea5386312b13171a044cd0a860550ad8
funiris <162334368+funiris@users.noreply.github.com>
2024-03-19 22:47:25 +0800

    main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,58 +3,21 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // Test adding a valid student
-    const myClass = new MyClass();
-    const student1 = new Student();
-    assert.strictEqual(myClass.addStudent(student1), 0);
-    assert.strictEqual(myClass.students.length, 1);
-    assert.strictEqual(myClass.students[0], student1);
-
-    // Test adding another valid student
-    const student2 = new Student();
-    assert.strictEqual(myClass.addStudent(student2), 1);
-    assert.strictEqual(myClass.students.length, 2);
-    assert.strictEqual(myClass.students[1], student2);
-
-    // Test adding an invalid student
-    assert.strictEqual(myClass.addStudent({}), -1);
-    assert.strictEqual(myClass.students.length, 2); // Should not add invalid student
+    // TODO
+    throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
-    // Test getting student by valid id
-    const myClass = new MyClass();
-    const student1 = new Student();
-    const student2 = new Student();
-
-    myClass.addStudent(student1);
-    myClass.addStudent(student2);
-
-    assert.strictEqual(myClass.getStudentById(0), student1);
-    assert.strictEqual(myClass.getStudentById(1), student2);
-
-    // Test getting student by invalid id
-    assert.strictEqual(myClass.getStudentById(-1), null);
-    assert.strictEqual(myClass.getStudentById(2), null);
+    // TODO
+    throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
-    // Test setting valid name
-    const student = new Student();
-    student.setName("CHINA");
-    assert.strictEqual(student.name, "CHINA");
-
-    // Test setting invalid name
-    student.setName(8081);
-    assert.strictEqual(student.name, "CHINA"); // Should not change name if invalid
+    // TODO
+    throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
-    // Test getting name when name is not set
-    const student = new Student();
-    assert.strictEqual(student.getName(), "");
-
-    // Test getting name when name is set
-    student.setName("CUBA");
-    assert.strictEqual(student.getName(), "CUBA");
+    // TODO
+    throw new Error("Test not implemented");
 });
